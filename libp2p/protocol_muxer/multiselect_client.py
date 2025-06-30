404110aa9ee2ec678dd558423968b29102de5834
Akash Mondal <akmo3901@gmail.com>
2025-06-30 11:16:08 +0000

    fix: initial connection succesfull

--- libp2p/protocol_muxer/multiselect_client.py
+++ libp2p/protocol_muxer/multiselect_client.py
@@ -145,6 +145,7 @@ class MultiselectClient(IMultiselectClient):
         except MultiselectCommunicatorError as error:
             raise MultiselectClientError() from error
 
+        print("Response: ", response)
         if response == protocol:
             return protocol
         if response == PROTOCOL_NOT_FOUND_MSG:
